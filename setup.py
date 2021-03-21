535b401ecfc365403c33de7999288363199bc3da
Alex Priest <80467372+prie0075@users.noreply.github.com>
2021-03-21 16:24:21 +1030

    Add initial files for C2

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.prie0075',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.prie0075\n\nA docassemble extension.\n\n## Author\n\nAlex Priest, prie0075@flinders.edu.au\n\n',
+      long_description='# docassemble.prie0075\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nAlex Priest, prie0075@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Alex Priest',
       author_email='prie0075@flinders.edu.au',
